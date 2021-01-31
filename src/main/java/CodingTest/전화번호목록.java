1fb2db3d6ac6e5dabca08d757d9b5964782b7548
KJJ <dkansk924@naver.com>
2021-01-31 20:18:12 +0900

    프로그래머스 전화번호 목록 풀이.

---
+++ src/main/java/CodingTest/전화번호목록.java
@@ -0,0 +1,17 @@
+package CodingTest;
+import java.util.*;
+import java.util.stream.Collectors;
+public class 전화번호목록 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        for (String s : phone_book) {
+            List<String> list = Arrays.stream(phone_book).filter(s1 -> !s1.equals(s)).collect(Collectors.toList());
+            for (String s1 : list) {
+                if(s1.startsWith(s)){
+                    return false;
+                }
+            }
+        }
+        return answer;
+    }
+}
